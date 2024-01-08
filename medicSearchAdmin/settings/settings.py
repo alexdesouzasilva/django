6c1d7a2a363dfc268f8b312e03f35185220e9380
Alex <alexdesouzasilva94@gmail.com>
2024-01-08 19:11:13 -0300

    add caminho dos arquivos estáticos

--- medicSearchAdmin/settings/settings.py
+++ medicSearchAdmin/settings/settings.py
@@ -107,6 +107,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#Caminho dos arquivos estáticos
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
