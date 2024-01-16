84d0090e2758d010a96b571e25222b644624f340
Alex <alexdesouzasilva94@gmail.com>
2024-01-16 19:37:47 -0300

    Customizando Painel Admin - incluindo filtro por data

--- medicSearch/admin.py
+++ medicSearch/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import *
 
+#criando classe e add atributo de filtro por data de criação
+class ProfileAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_at'
+
 # Register your models here.
-admin.site.register(Profile)
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(State)
 admin.site.register(City)
 admin.site.register(Neighborhood)
