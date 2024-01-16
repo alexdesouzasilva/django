dc082ae386953988c01eac0fa6c5071ab63e99c8
Alex <alexdesouzasilva94@gmail.com>
2024-01-16 19:56:50 -0300

    customizando painel admin

--- medicSearch/admin.py
+++ medicSearch/admin.py
@@ -3,7 +3,15 @@ from .models import *
 
 #criando classe e add atributo de filtro por data de criação
 class ProfileAdmin(admin.ModelAdmin):
+    search_fields = ('user__username',)
+    readonly_fields = ('user',)
+    exclude = ('favorites', 'created_at', 'update_at',)
     date_hierarchy = 'created_at'
+    list_display = ('user', 'role', 'birth', 'specialtiesList', 'addressesList',)
+    list_display_links = ('user', 'role',)
+    empty_value_display = '----'
+    list_filter = ('user__is_active', 'role')
+
 
 # Register your models here.
 admin.site.register(Profile, ProfileAdmin)
