31a74f2e7bd055ec2437a2929af8ac6deb105bfe
Alex <alexdesouzasilva94@gmail.com>
2024-01-20 14:53:47 -0300

    adicionando path de ProfileUrls no arquivo de urls principal, ou seja, no projeto admin

--- medicSearchAdmin/urls.py
+++ medicSearchAdmin/urls.py
@@ -24,5 +24,6 @@ from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     #Adicionando URLs da camanda view
-    path('', include('medicSearch.urls.HomeUrls'))
+    path('', include('medicSearch.urls.HomeUrls')),
+    path('profile/', include('medicSearch.urls.ProfileUrls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
