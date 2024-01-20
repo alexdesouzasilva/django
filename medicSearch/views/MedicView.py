51b5485a8491f9e729dc35e0886c73a5a87be915
Alex <alexdesouzasilva94@gmail.com>
2024-01-20 15:43:45 -0300

    capturando parâmetros passados na requisção via URL nas chamadas a medic

--- medicSearch/views/MedicView.py
+++ medicSearch/views/MedicView.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
 
 def list_medics_view(request):
+    name = request.GET.get("name")
+    speciality = request.GET.get("speciality")
+    neighborhood = request.GET.get("neighborhood")
+    city = request.GET.get("city")
+    state = request.GET.get("state")
+
     return HttpResponse('Listagem de 1 ou mais médicos')
