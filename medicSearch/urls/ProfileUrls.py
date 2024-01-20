c55422c004956d132c1b0e4f6f9fa4b35b1c156f
Alex <alexdesouzasilva94@gmail.com>
2024-01-20 14:57:24 -0300

    ajustando ProfileView, pois não deve ter espaço no parâmetro int:id e faltou vírgula, pois está sendo configurado mais de um path

--- medicSearch/urls/ProfileUrls.py
+++ medicSearch/urls/ProfileUrls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from medicSearch.views.ProfileView import list_profile_view
 
 urlpatterns = [
-    path("", list_profile_view)
-    path("<int: id>", list_profile_view)
+    path("", list_profile_view),
+    path("<int:id>", list_profile_view)
 ]
--- medicSearch/views/ProfileView.py
+++ medicSearch/views/ProfileView.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def list_profile_view(request, id=None):
-    return HttpResponse('<h1>Usuário de id %s!</h1>' % id)
+    return HttpResponse('<h1>Usuário de id %s!</h1>' %id)
