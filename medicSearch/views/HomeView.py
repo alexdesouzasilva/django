7110d991a5e85f306c789549a299de5c53096c7e
Alex <alexdesouzasilva94@gmail.com>
2024-01-23 18:54:54 -0300

    adicionando lib shortcuts para utilizar função render e reenderizar template home.html nas chamadas a HomeView

--- medicSearch/views/HomeView.py
+++ medicSearch/views/HomeView.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home_view(request):
-    return HttpResponse('<h1>Hello, Maria Eduarda!! </h1>', status=200)
+    return render(request, 'home/home.html', status=200)
