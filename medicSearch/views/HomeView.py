a38c26050f602e75dd3252bdbc91ebb7206fadda
Alex <alexdesouzasilva94@gmail.com>
2024-01-24 19:36:35 -0300

    adicionando formulário de busca de médicos na página home.html

--- medicSearch/views/HomeView.py
+++ medicSearch/views/HomeView.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 
 def home_view(request):
-    return render(request, 'home/home.html', status=200)
+    return render(request, template_name='home/home.html', status=200)
