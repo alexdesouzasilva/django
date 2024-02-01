7c6edd3ff61a95b74f1546bb0c078323f352bb01
Alex <alexdesouzasilva94@gmail.com>
2024-02-01 19:30:52 -0300

    add função que adiciona favorito

--- medicSearch/views/MedicView.py
+++ medicSearch/views/MedicView.py
@@ -1,5 +1,5 @@
 from medicSearch.models.Profile import Profile
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.db.models import Q
 from django.core.paginator import Paginator
 
@@ -56,3 +56,44 @@ def list_medics_view(request):
     }
 
     return render(request, template_name='medic/medics.html', context=context, status=200)
+
+
+def add_favorite_view(request):
+    page = request.POST.get("page")
+    name = request.POST.get("name")
+    speciality = request.POST.get("speciality")
+    neighborhood = request.POST.get("neighborhood")
+    city = request.POST.get("city")
+    state = request.POST.get("state")
+    id = request.POST.get("id")
+
+    try:
+        profile = Profile.objects.filter(user=request.user).first()
+        medic = Profile.objects.filter(user__id=id).first()
+        profile.favorites.add(medic.user)
+        profile.save()
+        msg = "Favorito adicionado com sucesso"
+        _type = "sucess"
+    except Exception as e:
+        print("Error %s" % e)
+        msg = "Um erro ocorreu ao salvar o médico nos favoritos"
+        _type = "danger"
+
+    if page:
+        arguments = "?page=%s" % (page)
+    else:
+        arguments = "?page=1"
+    if name:
+        arguments += "&name=%s" % name
+    if speciality:
+        arguments += "&speciality=%s" % speciality
+    if neighborhood:
+        arguments += "&neighborhood=%s" % neighborhood
+    if city:
+        arguments += "&city=%s" % city
+    if state:
+        arguments += "&state=%s" % state
+
+    arguments += "&msg=%s&type=%s" % (msg, _type)
+
+    return redirect(to='/medic/%s' % arguments)
